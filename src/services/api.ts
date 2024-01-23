edea6fc98e2f0a2693e32807df4a57fa74257727
Lucas B <lucas@skyloud.fr>
2024-01-23 14:19:41 +0100

    feat: use token instead of keycloak lib

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { keycloak } from "./keycloak";
 
 const instance = axios.create({
   withCredentials: true,
@@ -7,12 +6,13 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use((config: any) => {
+  const token = localStorage.getItem('auth_token');
   const request = {
     ...config,
     method: config.method,
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': keycloak.token,
+      'Authorization': token,
     }
   };
 
