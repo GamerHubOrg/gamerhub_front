58d0bcd80f65ab077db5d2bf437b0e95279161fd
Lucas Boinet <boinet1906@gmail.com>
2024-01-25 09:25:13 +0100

    feat: load env in config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router/index';
 import './style.css'
 import App from './App.vue'
 import { keycloak } from './services/keycloak';
-import 'dotenv/config';
 
 const store = createPinia();
 const app = createApp(App);
