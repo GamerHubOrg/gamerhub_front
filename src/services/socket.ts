0d13ee256ce7c842cfbdb21d88f3fbe02e0286eb
KenTay7 <johnkta7@gmail.com>
2024-01-26 19:04:45 +0100

    Lobbu

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,29 +1,34 @@
 import { reactive } from "vue";
 import { io } from "socket.io-client";
+import { keycloak } from "./keycloak";
 
 export const state = reactive({
-    connected: false,
-    room: ""
+  connected: false,
+  room: "",
 });
-
 
 const URL = import.meta.env.VITE_API_URL;
 
-const socket = io(URL);
+const socket = io(URL, {
+  extraHeaders: {
+    Authorization: keycloak.token,
+  },
+});
 
 function onConnect() {
-    console.log("Vous êtes connecté.");
-    state.connected = true;
+  console.log("Vous êtes connecté.");
+  state.connected = true;
 }
 
 function onDisconnect() {
-    state.connected = false;
-    console.log("Vous avez été déconnecté.");
+  state.connected = false;
+  state.room = "";
+  console.log("Vous avez été déconnecté.");
 }
 
 function onRoomCreated(roomId: string) {
-    state.room = roomId;
-    console.log("Vous avez rejoins la room " + roomId)
+  state.room = roomId;
+  console.log("Vous avez rejoins la room " + roomId);
 }
 
 socket.on("connect", onConnect);
@@ -31,7 +36,7 @@ socket.on("disconnect", onDisconnect);
 socket.on("room:created", onRoomCreated);
 
 socket.on("connect_error", (err) => {
-    console.log(`[socket.io] : Error : ${err.message}`);
+  console.log(`[socket.io] : Error : ${err.message}`);
 });
 
 export default socket;
