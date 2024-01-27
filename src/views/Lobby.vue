e5f689c5d0c46eb7ee7f6b430f116b21538741c2
KenTay7 <johnkta7@gmail.com>
2024-01-27 20:38:37 +0100

    Room leave handle and identifies owner and me

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -17,19 +17,25 @@ const isRoomInit = ref(false);
 const getRoomId = () => state.room;
 const getRoomUsers = () => state.data?.users;
 const currentUser = computed(() => store.getCurrentUser)
+
 // Fonction qui envoie un évenement au serveur
 
-const createRoom = () => {
+const handleCreateRoom = () => {
     console.log("createRoom", currentUser.value)
     socket.emit("room:create", gameName, currentUser.value)
 }
 
-const joinRoom = () => {
+const handleJoinRoom = () => {
     socket.emit("room:join", roomId, currentUser.value)
 }
 
-const startGame = () => {
+const handleStartGame = () => {
     socket.emit("room:start", getRoomId())
+}
+
+function handleLeaveRoom() {
+    console.log("should leave room")
+    socket.emit("room:leave", roomId);
 }
 
 // Fonction qui recoivent un évenement du serveur
@@ -48,16 +54,25 @@ function onRoomJoined(roomId: string, data: IRoomData) {
     state.data = data;
 }
 
-function onRoomLeave(roomId: string, data: IRoomData) {
-    console.log("You have leaved the room", data);
-
-    state.room = roomId;
+function onRoomUpdate(data: IRoomData) {
     state.data = data;
 }
+
+function onRoomLeft() {
+    isRoomInit.value = false;
+    state.room = "";
+    state.data = {};
+}
+
 
 function onRoomStarted(config: IRoomConfig) {
     state.data.config = config;
     alert("La partie a commencé !")
+}
+
+function onRoomNotFound(roomId: string) {
+    console.log(`La room ${roomId} n'existe pas`);
+
 }
 
 function onUserNotAuth() {
@@ -68,12 +83,14 @@ function onUserNotAuth() {
 
 socket.on("room:created", onRoomCreated);
 socket.on("room:joined", onRoomJoined);
-socket.on("room:leave", onRoomLeave);
+socket.on("room:update", onRoomUpdate);
+socket.on("room:left", onRoomLeft);
 socket.on("room:started", onRoomStarted);
-socket.on("room:not-found", (roomId: string) => console.log(`La room ${roomId} n'existe pas`))
+socket.on("room:not-found", onRoomNotFound)
 socket.on("user:not-auth", onUserNotAuth)
 socket.on("room:deleted", (roomId: string) => {
     state.room = "";
+    state.data = {};
     alert(`La room ${roomId} a été supprimée.`);
 })
 
@@ -84,49 +101,50 @@ watch(
         if (!currentUser.value || isRoomInit.value) return;
 
         console.log("watch", roomId)
-        if (!roomId) createRoom();
-        else joinRoom();
+        if (!roomId) handleCreateRoom();
+        else handleJoinRoom();
 
         isRoomInit.value = true;
     }
 )
 
-
-function handleDisconnectRoom() {
-    console.log("should leave room")
-    socket.emit("room:leave", roomId);
-}
-
 watch(
     () => route.query?.roomId,
     (newRoom, oldRoom) => {
+        console.log("old", oldRoom);
+        console.log("new", newRoom);
+
         if (oldRoom && (!newRoom || newRoom !== oldRoom)) {
-            handleDisconnectRoom()
+            handleLeaveRoom()
         }
     }
 )
 
 onMounted(() => {
-    window.addEventListener('beforeunload', handleDisconnectRoom)
+    window.addEventListener('beforeunload', handleLeaveRoom)
 })
 
 onBeforeUnmount(() => {
-    window.removeEventListener('beforeunload', handleDisconnectRoom)
+    window.removeEventListener('beforeunload', handleLeaveRoom)
 })
 
 </script>
 
 <template>
-    <div class="flex gap-10 flex-wrap">
+    <div v-if="state.room" class="flex gap-10 flex-wrap">
         <div class="flex-1 flex flex-col max-w-[600px] mx-auto gap-3">
             <h1 class="text-3xl">Configuration</h1>
             <input type="number" v-model="maxPlayers" placeholder="Entrez le nombre maximum de joueurs">
-            <button class="bg-green-500 text-black" @click="startGame">Lancer la partie</button>
+            <button class="bg-green-500 text-black" @click="handleStartGame">Lancer la partie</button>
         </div>
         <div class="flex-1 flex flex-col">
-            <div v-for="user in getRoomUsers()" class="bg-white text-black w-fit px-4 font-bold">
-                {{ user.username }}
+            <div v-for="user in getRoomUsers()" class="bg-white text-black w-fit px-4 font-bold"
+                :class="{ 'bg-red-500': currentUser?.email === user.email }">
+                <p>{{ user.username }} <span v-if="user.isOwner">(Owner)</span></p>
             </div>
         </div>
+    </div>
+    <div v-else>
+        <p>Cette room n'existe pas.</p>
     </div>
 </template>
