c43519361c369931ffb442860f31eed3a60f2805
Lucas Boinet <boinet1906@gmail.com>
2024-06-18 22:40:09 +0200

    fix: remove lobby mount

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -259,27 +259,27 @@ onMounted(() => {
                 </button>
             </div>
         </div>
-    </div>
-    <Modal 
-        :open="publishConfigModalOpen" 
-        @close="publishConfigModalOpen = false" 
-    >
-        <form @submit="handleShareConfig">
-            <div>
-                <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-dark5">
-                    <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
-                </div>
-                <div class="mt-3 text-center sm:mt-5">
-                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-white">Give this config a name !</DialogTitle>
-                    <div class="mt-10">
-                        <input v-model="shareConfigName" required type="text" class="block w-full p-1.5 rounded-md border-0 bg-dark5 shadow-sm ring-1 ring-inset ring-dark3 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" placeholder="Config name here..." />
+        <Modal 
+            :open="publishConfigModalOpen" 
+            @close="publishConfigModalOpen = false" 
+        >
+            <form @submit="handleShareConfig">
+                <div>
+                    <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-dark5">
+                        <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+                    </div>
+                    <div class="mt-3 text-center sm:mt-5">
+                        <DialogTitle as="h3" class="text-base font-semibold leading-6 text-white">Give this config a name !</DialogTitle>
+                        <div class="mt-10">
+                            <input v-model="shareConfigName" required type="text" class="block w-full p-1.5 rounded-md border-0 bg-dark5 shadow-sm ring-1 ring-inset ring-dark3 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" placeholder="Config name here..." />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                <button type="submit" class="inline-flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:col-start-2">Share</button>
-                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0" @click="publishConfigModalOpen = false" ref="cancelButtonRef">Cancel</button>
-            </div>
-        </form>
-    </Modal>
+                <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                    <button type="submit" class="inline-flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:col-start-2">Share</button>
+                    <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0" @click="publishConfigModalOpen = false" ref="cancelButtonRef">Cancel</button>
+                </div>
+            </form>
+        </Modal>
+    </div>
 </template>
