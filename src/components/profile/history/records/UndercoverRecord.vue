c03e039e464bbd8e9565b176e159ef544633f812
KenTay7 <johnkta7@gmail.com>
2024-07-07 23:33:36 +0200

    feat: Undercover record fix

--- src/components/profile/history/records/UndercoverRecord.vue
+++ src/components/profile/history/records/UndercoverRecord.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex max-[800px]:flex-col w-full h-full min-[801px]:gap-5 relative after:absolute after:left-0 after:top-0 after:h-full after:w-2 after:bg-primary"
-        :class="{ 'after:bg-red-500': record.campWin !== currentCamp}">
+        :class="{ 'after:bg-red-500': record.campWin !== currentCamp }">
         <div class="flex relative main">
             <div
                 class="text-xs min-[801px]:text-base flex h-full justify-between py-2 px-6 max-[800px]:items-center max-[800px]:w-full max-[800px]:justify-between min-[801px]:flex-col min-[801px]:w-[200px]">
@@ -14,15 +14,17 @@
             </div>
             <span class="max-[800px]:hidden h-full min-w-[1px] w-[1px] bg-white opacity-30"></span>
         </div>
-        <div class="flex flex-col justify-center items-center w-[100px]">
-            <img v-if="currentCamp === 'civilian'" :src="CivilianImage" class="h-16 w-16" />
-            <img v-if="currentCamp === 'undercover'" :src="UndercoverImage" class="h-16 w-16" />
-            <p class="font-bold">{{ capitalizeFirstLetter(currentCamp) }}</p>
-        </div>
-        <span class="max-[800px]:hidden h-full min-w-[1px] w-[1px] bg-white opacity-30"></span>
-        <div class="flex flex-col justify-center">
-           <p>Civilian word : {{ record.civilianWord }}</p>
-           <p>Undercover word : {{ record.spyWord }}</p>
+        <div class="max-[800px]:text-xs h-full min-[801px]:text-base flex gap-5 max-[800px]:pl-5">
+            <div class="flex flex-col justify-center items-center w-[100px]">
+                <img v-if="currentCamp === 'civilian'" :src="CivilianImage" class="h-16 w-16" />
+                <img v-if="currentCamp === 'undercover'" :src="UndercoverImage" class="h-16 w-16" />
+                <p class="font-bold">{{ capitalizeFirstLetter(currentCamp) }}</p>
+            </div>
+            <span class="h-full min-w-[1px] w-[1px] bg-white opacity-30"></span>
+            <div class="flex flex-1 flex-col justify-center">
+                <p class="line-clamp-2">Civilian word : {{ record.civilianWord }}</p>
+                <p>Undercover word : {{ record.spyWord }}</p>
+            </div>
         </div>
     </div>
 </template>
