6afc757e41943b028566d0c6b00ad55062391623
Lucas B <lucas@skyloud.fr>
2024-07-08 12:14:14 +0200

    fix: remove layout loader

--- src/layouts/Authenticated.vue
+++ src/layouts/Authenticated.vue
@@ -3,11 +3,8 @@
     <Navbar />
     <Lobby v-show="!isLobbyCollapsed"/>
 
-    <main v-if="!loading" class="p-4 h-full pt-20 xl:max-w-screen-xl w-full mx-auto">
-      <router-view v-if="!loading" />
-    </main>
-    <main v-else class="h-screen flex justify-center items-center">
-      <Loader />
+    <main class="p-4 h-full pt-20 xl:max-w-screen-xl w-full mx-auto">
+      <router-view />
     </main>
   </div>
 </template>
@@ -16,7 +13,6 @@
 import { computed, onMounted, ref } from 'vue';
 import Lobby from '../components/Lobby.vue';
 import Navbar from '../components/Navbar.vue';
-import Loader from '../components/Loader.vue';
 import {useAuthStore} from "../modules/auth/auth.store";
 import { useGamesStore } from '@/modules/games/games.store';
 
@@ -24,17 +20,14 @@ const store = useAuthStore();
 const gamesStore = useGamesStore();
 
 const isLobbyCollapsed = computed(() => gamesStore.isLobbyCollapsed);
-const loading = ref(false);
 
 async function handleSyncUserInfo() {
   try {
-    loading.value = true;
     await store.getMe()
 
   } catch (err) {
     store.setCurrentUser(undefined);
   }
-  loading.value = false;
 }
 
 onMounted(async () => {
