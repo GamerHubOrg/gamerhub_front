3add20204faee7c8b467537b03269af9ce135323
KenTay7 <johnkta7@gmail.com>
2024-07-09 23:01:15 +0200

    WIP: Premium + navbar translations

--- src/translations/en/homepage/game-list-item.translations.ts
+++
@@ -1,9 +0,0 @@
-import { IGameListItemTranslations } from "@/translations/types/homepage.translations.types";
-
-const enGameListItemTranslations: IGameListItemTranslations = {
-  players: "player | players",
-  notAvailable: "This game is not available for the moment.",
-  createRoom : "Create room"
-};
-
-export default enGameListItemTranslations;
