bc3d7a371b0b10c6cecded286bf558f5aabe3e40
KenTay7 <johnkta7@gmail.com>
2024-07-09 23:01:15 +0200

    WIP: Premium + navbar translations

--- src/translations/fr/homepage/game-list-item.translations.ts
+++
@@ -1,9 +0,0 @@
-import { IGameListItemTranslations } from "@/translations/types/homepage.translations.types";
-
-const frGameListItemTranslations: IGameListItemTranslations = {
-  players: "joueur | joueurs",
-  notAvailable : "Le jeu n'est pas encore disponible pour le moment.",
-  createRoom : "Créer une partie"
-};
-
-export default frGameListItemTranslations;
