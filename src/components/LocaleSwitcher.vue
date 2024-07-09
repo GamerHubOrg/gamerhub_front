aa2c3a264ce24d5d3256e1a2e8b09cc5464b453d
KenTay7 <johnkta7@gmail.com>
2024-07-09 20:54:59 +0200

    WIP: initialize i18n

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex gap-1 items-center text-sm">
+    <GlobeEuropeAfricaIcon class="h-5 w-5 text-gray-400" />
+    <select v-model="$i18n.locale" class="bg-transparent">
+      <option
+        class="bg-lightgrey"
+        v-for="locale in $i18n.availableLocales"
+        :key="`locale-${locale}`"
+        :value="locale"
+      >
+        {{ getLocaleLabel(locale) }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { GlobeEuropeAfricaIcon } from "@heroicons/vue/24/outline";
+
+const getLocaleLabel = (key: string) => {
+  switch (key) {
+    case "fr":
+      return "Français";
+    case "en":
+      return "English";
+    default:
+      return key;
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import router from './router/index';
-import 'vue3-toastify/dist/index.css';
-import Vue3Toastify from 'vue3-toastify';
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import router from "./router/index";
+import "vue3-toastify/dist/index.css";
+import Vue3Toastify from "vue3-toastify";
+import "./style.css";
+import App from "./App.vue";
+import { createI18n } from "vue-i18n";
+import enTranslations from "./translations/en";
+import frTranslations from "./translations/fr";
 
+const i18n = createI18n({
+  locale: navigator.language.split("-")[0],
+  fallbackLocale: "fr",
+  availableLocales : ["fr", "en"],
+  messages: {
+    en: enTranslations,
+    fr: frTranslations,
+  },
+});
 const store = createPinia();
 const app = createApp(App);
 
 app
+  .use(i18n)
   .use(router)
   .use(store)
-  .use(Vue3Toastify, {clearOnUrlChange: false})
-  .mount('#app');
+  .use(Vue3Toastify, { clearOnUrlChange: false })
+  .mount("#app");
