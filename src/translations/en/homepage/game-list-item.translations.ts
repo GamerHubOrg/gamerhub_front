82c0fb8f1f0bee2cbbd404da3dd53b0f62b15526
KenTay7 <johnkta7@gmail.com>
2024-07-09 20:54:59 +0200

    WIP: initialize i18n

---
+++ src/translations/en/homepage/game-list-item.translations.ts
@@ -0,0 +1,9 @@
+import { IGameListItemTranslations } from "@/translations/types/homepage.translations.types";
+
+const enGameListItemTranslations: IGameListItemTranslations = {
+  players: "player | players",
+  notAvailable: "This game is not available for the moment.",
+  createRoom : "Create room"
+};
+
+export default enGameListItemTranslations;
---
+++ src/translations/fr/homepage/index.ts
@@ -0,0 +1,8 @@
+import IHomepageTranslations from "../../types/homepage.translations.types";
+import frGameListItemTranslations from "./game-list-item.translations";
+
+const frHomepageTranslations: IHomepageTranslations = {
+  "game-list-item": frGameListItemTranslations,
+};
+
+export default frHomepageTranslations;
