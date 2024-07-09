fee7a8869cc884e5ee6e69f1da209be96e83af05
KenTay7 <johnkta7@gmail.com>
2024-07-09 20:54:59 +0200

    WIP: initialize i18n

---
+++ src/translations/fr/homepage/game-list-item.translations.ts
@@ -0,0 +1,9 @@
+import { IGameListItemTranslations } from "@/translations/types/homepage.translations.types";
+
+const frGameListItemTranslations: IGameListItemTranslations = {
+  players: "joueur | joueurs",
+  notAvailable : "Le jeu n'est pas encore disponible pour le moment.",
+  createRoom : "Créer une partie"
+};
+
+export default frGameListItemTranslations;
