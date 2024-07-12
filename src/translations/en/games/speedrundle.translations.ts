b8ecdca442b0fe4c4400cc5f41eeb7927da0fa5a
KenTay7 <johnkta7@gmail.com>
2024-07-12 14:08:38 +0200

    feat: GameLobby component on all games + langues in Lobby + Speedrundle config

--- src/translations/en/games/speedrundle.translations.ts
+++ src/translations/en/games/speedrundle.translations.ts
@@ -1,17 +1,39 @@
 import { ISpeedrundleTranslations } from "@/translations/types/games.translations.types";
 
-
 const enSpeedrundleTranslations: ISpeedrundleTranslations = {
-  record : {
+  record: {
     rank: "Rank",
-    me: "Me",
     unknown: "Unknown user",
     totalScore: "Total score",
     averageScore: "Average score",
     maxScore: "Max score",
     minScore: "Min score",
     charactersToGuess: "Characters to guess",
-  }
+  },
+  configs: {
+    mode: {
+      label: "Game mode",
+      values: {
+        classic: "Classic",
+      },
+    },
+    clues: "Clues",
+    theme: "Theme",
+    nbRounds: "Number of rounds",
+    pokemonGens: "Pokemon generations",
+  },
+  columns: {
+    league_of_legends: {
+      gender: "Gender",
+      species: "Species",
+      tags: "Combat",
+      ressource: "Ressource",
+      range: "Range",
+      position: "Position",
+      region: "Region",
+      releaseYear: "Release year",
+    },
+  },
 };
 
 export default enSpeedrundleTranslations;
