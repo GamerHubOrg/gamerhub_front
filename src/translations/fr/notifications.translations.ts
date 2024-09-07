843d0fd523afe99296dd98c570a72b7e4d5f4e6a
KenTay7 <johnkta7@gmail.com>
2024-09-07 15:25:25 +0200

    feat: notifications translate

---
+++ src/translations/fr/notifications.translations.ts
@@ -0,0 +1,25 @@
+import INotificationsTranslations from "../types/notifications.translations.types";
+
+const frNotificationsTranslations: INotificationsTranslations = {
+  room: {
+    gameStarted: "Cette partie est déjà lancée, veuillez attendre la fin.",
+    alreadyFull: "Cette partie a déjà atteint le nombre maximal de joueurs.",
+    notEnoughPlayers: "Il n y a plus assez de joueurs pour continuer la partie.",
+    userNotInRoom: "Cet utilisateur n'est pas dans le salon.",
+    ownerNotFound: "Impossible de trouver le propriétaire du salon.",
+    promoted: "Tu as été promu propriétaire du salon.",
+  },
+  speedrundle: {
+    selectTheme: "Veuillez sélectionner un thème pour la partie.",
+    selectClues: "Veuillez sélectionner au moins 1 indices.",
+    youGuessed: "Vous avez trouvé {name} !",
+    giveUp: "Vous avez abandonné ce tour, la réponse était {name} !",
+  },
+  undercover: {
+    playerEliminated: "",
+    youEliminated: "",
+    voteEqual: "",
+  },
+};
+
+export default frNotificationsTranslations;
